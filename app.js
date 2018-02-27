296d20ea61a68460b0a06445dbb160046c46569d
rayjune <ray_june@qq.com>
2018-02-27 16:26:22 +0800

    get what I want from passed JSON data

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const request = require('request');
 
 request({
-  url: 'http://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20phladelpha',
+  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20phladelpha',
   json: true
 }, (error, response, body) => {
-  // console.log(error);
-  // console.log(response);
-  console.log(JSON.stringify(body, undefined, 2));
+  // console.log(JSON.stringify(response, undefined, 2));
+  // console.log(JSON.stringify(body, undefined, 2));
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);    
 });
